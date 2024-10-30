a0eb8bcf8868aa94544260b9c9eb3f543eb88ce2
Ericsson Budhilaw <ericsson.budhilaw@gmail.com>
2024-10-30 23:17:47 +0700

    chore(): fix

--- php/flake.nix
+++ php/flake.nix
@@ -1,10 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
-
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   nixConfig = {
@@ -12,31 +10,34 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, devenv, ... }@inputs: inputs.flake-utils.lib.eachSystem [
-    "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"
-  ] (system:
-    let pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-    in {
-      packages.devenv-up = self.devShells.${system}.default.config.procfileScript;
-      packages.devenv-test = self.devShells.${system}.default.config.test;
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
 
-      devShells.${system}.default = devenv.lib.mkShell {
-        inherit inputs pkgs;
-        modules = [
-          ({ pkgs, config, ... }: {
-            # This is your devenv configuration
-            packages = [ pkgs.hello ];
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [ {
+                  packages = [ pkgs.hello ];
 
-            enterShell = ''
-              hello
-            '';
+                  enterShell = ''
+                    hello
+                  '';
 
-            processes.run.exec = "hello";
-          })
-        ];
-      };
-   });
+                  processes.run.exec = "hello";
+                }
+              ];
+            };
+          });
+    };
 }
