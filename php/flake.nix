753e4cdacfbd496dfd42292aaccaaac86ffd5555
Ericsson Budhilaw <ericsson.budhilaw@gmail.com>
2024-10-30 22:56:31 +0700

    chore(): update system to darwin

--- php/flake.nix
+++ php/flake.nix
@@ -11,7 +11,7 @@
 
   outputs = { self, nixpkgs, devenv, ... } @ inputs:
     let
-      system = "x86_64-linux";
+      system = "aarch64-darwin";
 
       pkgs = nixpkgs.legacyPackages.${system};
     in
