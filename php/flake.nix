6b127566c3e3e199d050813bf0823753df051157
Ericsson Budhilaw <ericsson.budhilaw@gmail.com>
2024-10-30 23:09:36 +0700

    chore(): fix

--- php/flake.nix
+++ php/flake.nix
@@ -1,9 +1,10 @@
 {
-  description = "A Nix-flake-based Go development environment";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    devenv.url = "github:cachix/devenv";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   nixConfig = {
@@ -11,30 +12,31 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, utils, devenv, ... } @ inputs:
-
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit inputs system; };
-      in
-      {
-        packages.${system}.devenv-up = self.devShells.${system}.default.config.procfileScript;
-        packages.${system}.devenv-test = self.devShells.${system}.default.config.test;
-
-        devShells.default = devenv.lib.mkShell {
-          inherit inputs pkgs;
-          modules = [
-            ({ pkgs, config, ... }: {
-              # This is your devenv configuration
-              packages = [ pkgs.hello ];
-
-              enterShell = ''
-                hello
-              '';
-
-              processes.run.exec = "hello";
-            })
-          ];
+  outputs = { self, nixpkgs, ... }@input: input.flake-utils.lib.eachSystem [
+    "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"
+  ] (system:
+    let pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
         };
-      });
+    in {
+      packages.${system}.devenv-up = self.devShells.${system}.default.config.procfileScript;
+      packages.${system}.devenv-test = self.devShells.${system}.default.config.test;
+
+      devShells.${system}.default = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          ({ pkgs, config, ... }: {
+            # This is your devenv configuration
+            packages = [ pkgs.hello ];
+
+            enterShell = ''
+              hello
+            '';
+
+            processes.run.exec = "hello";
+          })
+        ];
+      }
+    });
 }
