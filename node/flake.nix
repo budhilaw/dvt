027e057d4616d7134e7e4904a43d9b9794b88990
Ericsson Budhilaw <ericsson.budhilaw@gmail.com>
2024-12-16 09:18:02 +0700

    chore(): changes node

--- node/flake.nix
+++ node/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A Nix-flake-based Go development environment";
+  description = "A Nix-flake-based Node.js development environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -11,22 +11,27 @@
     utils.lib.eachDefaultSystem (system:
       let
         nodejsVersion = 18;
-        overlays = [ (final: prev: rec {
+        overlays = [
+          (final: prev: rec {
             nodejs = prev."nodejs-${toString nodejsVersion}_x";
             pnpm = prev.nodePackages.pnpm;
             yarn = (prev.yarn.override { inherit nodejs; });
           })
         ];
+
         pkgs = import nixpkgs { inherit overlays system; };
 
       in
       {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nodejs
-            pnpm
-            yarn
-          ];
+          buildInputs = with pkgs; [ nodejs pnpm yarn ];
+
+          shellHook = with pkgs;''
+            echo "node `${nodejs}/bin/node --version`"
+            echo "yarn `${yarn}/bin/yarn --version`"
+            echo "pnpm `${pnpm}/bin/pnpm --version`"
+          '';
         };
+
       });
 }
