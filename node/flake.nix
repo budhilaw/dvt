e81a5b1c21e0a968d7ad95da54f86eb4ac375a2d
Ericsson Budhilaw <ericsson@budhilaw.com>
2025-08-07 12:10:59 +0700

    chore(): update node flake for flexible

--- node/flake.nix
+++ node/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A Nix-flake-based Node.js development environment";
+  description = "A flexible Nix-flake-based Node.js development environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -10,27 +10,37 @@
 
     utils.lib.eachDefaultSystem (system:
       let
-        nodejsVersion = 24;
-        overlays = [
-          (final: prev: rec {
-            nodejs = prev."nodejs-${toString nodejsVersion}_x";
-            pnpm = prev.nodePackages.pnpm;
-            yarn = (prev.yarn.override { inherit nodejs; });
-          })
-        ];
+        mkNodejsShell = nodejsVersion:
+          let
+            overlays = [
+              (final: prev: rec {
+                nodejs = prev."nodejs_${toString nodejsVersion}";
+                pnpm = prev.nodePackages.pnpm;
+                yarn = (prev.yarn.override { inherit nodejs; });
+              })
+            ];
 
-        pkgs = import nixpkgs { inherit overlays system; };
+            pkgs = import nixpkgs { inherit overlays system; };
+          in
+          pkgs.mkShell {
+            buildInputs = with pkgs; [ nodejs pnpm yarn ];
+
+            shellHook = with pkgs;''
+              echo "Node.js ${toString nodejsVersion} development environment"
+              echo "node `${nodejs}/bin/node --version`"
+              echo "yarn `${yarn}/bin/yarn --version`"
+              echo "pnpm `${pnpm}/bin/pnpm --version`"
+            '';
+          };
 
       in
       {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ nodejs pnpm yarn ];
-
-          shellHook = with pkgs;''
-            echo "node `${nodejs}/bin/node --version`"
-            echo "yarn `${yarn}/bin/yarn --version`"
-            echo "pnpm `${pnpm}/bin/pnpm --version`"
-          '';
+        devShells = {
+          default = mkNodejsShell 22;  # Default to Node.js 22 LTS
+          "18" = mkNodejsShell 18;
+          "20" = mkNodejsShell 20;
+          "22" = mkNodejsShell 22;
+          "24" = mkNodejsShell 24;
         };
 
       });
